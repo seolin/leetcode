c2ff591e38f1a48d158ef1efb90fa2e20f03a010
kkchen <liangzi.ckk@raycloud.com>
2020-10-31 16:01:46 +0800

    修改 1. 两数之和

--- com/seolin/question/question1/Solution1.java
+++ com/seolin/question/question1/Solution1.java
@@ -1,5 +1,6 @@
 package com.seolin.question.question1;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,36 +19,16 @@ public class Solution1 {
     public static void main(String[] args) {
         int[] one = {2, 7, 11, 15};
         int target = 9;
-        int[] towsum = towSum2(one, target);
-        for (int i : towsum) {
-            System.out.println(i + ",");
-        }
+        Arrays.stream(new Solution2().towSum(one, target)).forEach(System.out::println);
     }
 
-    public static int[] towSum(int[] nums, int target) {
+    public int[] towSum(int[] nums, int target) {
         Map<Integer, Integer> resultMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (resultMap.containsKey(target - nums[i])) {
                 return new int[]{resultMap.get(target - nums[i]), i};
             } else {
                 resultMap.put(nums[i], i);
-            }
-        }
-        return null;
-    }
-
-    /**
-     * 算法复杂度 n的平方
-     * @param nums
-     * @param target
-     * @return
-     */
-    public static int[] towSum2(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
             }
         }
         return null;
---
+++ com/seolin/question/question1/Solution2.java
@@ -0,0 +1,32 @@
+package com.seolin.question.question1;
+
+import java.util.Arrays;
+
+/**
+ * @author liangzi on 2020/10/31
+ */
+public class Solution2 {
+    /**
+     * 算法复杂度 n的平方
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] towSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] one = {2, 7, 11, 15};
+        int target = 9;
+        Arrays.stream(new Solution2().towSum(one, target)).forEach(System.out::println);
+    }
+}
