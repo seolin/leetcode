fcf8d379d23758c56fb5a4d0189818f6488beaaa
kkchen <liangzi.ckk@raycloud.com>
2020-11-02 21:32:00 +0800

    91. 解码方法

---
+++ com/seolin/question/question1000/question1t100/question91/Solution.java
@@ -0,0 +1,106 @@
+package com.seolin.question.question1000.question1t100.question91;
+//一条包含字母 A-Z 的消息通过以下方式进行了编码：
+//
+// 'A' -> 1
+//'B' -> 2
+//...
+//'Z' -> 26
+//
+//
+// 给定一个只包含数字的非空字符串，请计算解码方法的总数。
+//
+// 题目数据保证答案肯定是一个 32 位的整数。
+//
+//
+//
+// 示例 1：
+//
+// 输入："12"
+//输出：2
+//解释：它可以解码为 "AB"（1 2）或者 "L"（12）。
+//
+//
+// 示例 2：
+//
+// 输入："226"
+//输出：3
+//解释：它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+//
+//
+// 示例 3：
+//
+// 输入：s = "0"
+//输出：0
+//
+//
+// 示例 4：
+//
+// 输入：s = "1"
+//输出：1
+//
+//
+// 示例 5：
+//
+// 输入：s = "2"
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= s.length <= 100
+// s 只包含数字，并且可以包含前导零。
+//
+// Related Topics 字符串 动态规划
+
+/**
+ * 91. 解码方法
+ *
+ * @author liangzi on 2020/11/2
+ */
+public class Solution {
+    public int numDecodings(String s) {
+        char[] words = s.toCharArray();
+        if (words[0] == '0') {
+            return 0;
+        }
+        int n = words.length;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (i == 1) {
+                if (words[i] == '0') {
+                    if (words[i - 1] == '1' || words[i - 1] == '2') {
+                        dp[i] = 1;
+                    } else {
+                        return 0;
+                    }
+                } else if (words[i - 1] == '1' || (words[i - 1] == '2' && words[i] >= '1' && words[i] <= '6')) {
+                    dp[i] = dp[i - 1] + 1;
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            } else {
+                if (words[i] == '0') {
+                    if (words[i - 1] == '1' || words[i - 1] == '2') {
+                        dp[i] = dp[i - 2];
+                    } else {
+                        return 0;
+                    }
+                } else if (words[i - 1] == '1' || (words[i - 1] == '2' && words[i] >= '1' && words[i] <= '6')) {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            }
+        }
+        return dp[s.length() - 1];
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().numDecodings("2101"));
+    }
+}
