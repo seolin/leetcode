000f9923adc7648ee74ff28496c63dadfc79a461
kkchen <liangzi.ckk@raycloud.com>
2020-11-19 13:03:09 +0800

    283. 移动零 双指针

---
+++ com/seolin/question/question1000/question201t300/question283/Solution1.java
@@ -0,0 +1,42 @@
+package com.seolin.question.question1000.question201t300.question283;
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+// 示例:
+//
+// 输入: [0,1,0,3,12]
+//输出: [1,3,12,0,0]
+//
+// 说明:
+//
+//
+// 必须在原数组上操作，不能拷贝额外的数组。
+// 尽量减少操作次数。
+//
+// Related Topics 数组 双指针
+// 👍 834 👎 0
+
+
+/**
+ * 283. 移动零
+ *
+ * @author liangzi on 2020/11/19
+ */
+public class Solution1 {
+    public void moveZeroes(int[] nums) {
+        //用双指针
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
