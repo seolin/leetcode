511a9e439a38243528dd3e9a80d39f95cff8ed99
kkchen <liangzi.ckk@raycloud.com>
2020-12-22 20:53:10 +0800

    102. 二叉树的层序遍历

---
+++ com/seolin/question/question1000/question101t200/question102/Solution.java
@@ -0,0 +1,62 @@
+package com.seolin.question.question1000.question101t200.question102;
+//给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+//
+// 例如：
+//给定二叉树 [3,9,20,null,null,15,7],
+//
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回锯齿形层序遍历如下：
+//
+//
+//[
+//  [3],
+//  [20,9],
+//  [15,7]
+//]
+//
+// Related Topics 栈 树 广度优先搜索
+// 👍 354 👎 0
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. 二叉树的层序遍历
+ *
+ * @author liangzi on 2020/12/22
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> subList = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode current = queue.poll();
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+                subList.add(current.val);
+            }
+            result.add(subList);
+        }
+        return result;
+    }
+}
