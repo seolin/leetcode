c71270fa6f8295bb09627683bf5e89d2d412d3da
chenkangkang <chenkangkang792@hellobike.com>
2022-05-08 10:15:19 +0800

    init

---
+++ com/seolin/beat/T23/T23.java
@@ -0,0 +1,86 @@
+package com.seolin.beat.T23;
+
+//给你一个链表数组，每个链表都已经按升序排列。
+//
+// 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+//
+//
+//
+// 示例 1：
+//
+// 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+//输出：[1,1,2,3,4,4,5,6]
+//解释：链表数组如下：
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//将它们合并到一个有序链表中得到。
+//1->1->2->3->4->4->5->6
+//
+//
+// 示例 2：
+//
+// 输入：lists = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+// 输入：lists = [[]]
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// k == lists.length
+// 0 <= k <= 10^4
+// 0 <= lists[i].length <= 500
+// -10^4 <= lists[i][j] <= 10^4
+// lists[i] 按 升序 排列
+// lists[i].length 的总和不超过 10^4
+//
+// Related Topics 链表 分治 堆（优先队列） 归并排序 👍 1936 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * @author chenkangkang
+ * @date 2022/5/7 5:02 PM
+ **/
+public class T23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return partition(lists, 0, lists.length - 1);
+    }
+
+    public ListNode partition(ListNode[] lists, int l, int r) {
+        if (l == r) {
+            return lists[r];
+        }
+        int mid = (l + r) >> 1;
+        ListNode l1 = partition(lists, l, mid);
+        ListNode l2 = partition(lists, mid + 1, r);
+        return merge(l1, l2);
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val > l2.val) {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        } else {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        }
+    }
+}
---
+++ com/seolin/beat/TreeNode.java
@@ -0,0 +1,16 @@
+package com.seolin.beat;
+
+/**
+ * 树的节点
+ *
+ * @author liangzi on 2020/10/29
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
